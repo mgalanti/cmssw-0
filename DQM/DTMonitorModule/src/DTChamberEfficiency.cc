7bad9ee389d31021da9599b7b5de6bce21d454e3
Mario <mario.galanti@cern.ch>
2013-10-14 15:59:39 +0200

    More changes to the MuonDetLayerMeasurements interface

--- DQM/DTMonitorModule/src/DTChamberEfficiency.cc
+++ DQM/DTMonitorModule/src/DTChamberEfficiency.cc
@@ -82,8 +82,10 @@ DTChamberEfficiency::DTChamberEfficiency(const ParameterSet& pSet)
   thedt4DSegments = pSet.getParameter<InputTag>("dt4DSegments");
   thecscSegments = pSet.getParameter<InputTag>("cscSegments");
 
+  edm::ConsumesCollector iC = consumesCollector();
+  
   theMeasurementExtractor = new MuonDetLayerMeasurements(thedt4DSegments,thecscSegments,
-                                                         labelRPCRecHits,consumesCollector(),
+                                                         labelRPCRecHits,iC,
                                                          true,false,false); 
 
   theNavigationType = pSet.getParameter<string>("NavigationType");
